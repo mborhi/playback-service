9735335d77ae9a382819ed50a81a6c8e6a471c48
Marcell Borhi <mborhi@iu.edu>
2022-08-13 19:13:01 -0400

    install packages

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    setupFiles: ['dotenv/config'],
+    testPathIgnorePatterns: ['/node_modules/', '/build/'],
+    watchPathIgnorePatterns: ['globalConfig']
+};
