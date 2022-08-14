433b110697773419a45f20ea64faae4e1c2594cc
Marcell Borhi <mborhi@iu.edu>
2022-08-14 02:07:37 -0400

    add pause endpoint to routes

--- src/routes/playback.ts
+++ src/routes/playback.ts
@@ -1,10 +1,11 @@
 import express from 'express';
+import { pauseSong } from '../utils/pause-utils';
 import { playSong } from '../utils/play-utils';
 import { changeTrackVolume } from '../utils/volume-utils';
 
 const router = express.Router();
 
-router.get('/play', async (req, res) => {
+router.put('/play', async (req, res) => {
     const { device_id, track_uri } = req.query; // validated at api gateway 
     const access_token = req.headers.access_token as string; // validated at api gateway
     try {
@@ -20,8 +21,20 @@ router.get('/play', async (req, res) => {
     }
 });
 
-router.get('/pause', async (req, res) => {
-
+router.put('/pause', async (req, res) => {
+    const { device_id } = req.query; // validated at api gateway 
+    const access_token = req.headers.access_token as string; // validated at api gateway
+    try {
+        const result = await pauseSong(access_token, device_id as string);
+        res.send(result);
+    } catch (error) {
+        res.status(500).json({
+            "error": {
+                "status": 500,
+                "message": "Internal server error"
+            }
+        });
+    }
 });
 
 router.put('/volume', async (req, res) => {
