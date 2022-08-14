4a6e142bcca6a4522d083f3d34a23fe026e355c5
Marcell Borhi <mborhi@iu.edu>
2022-08-13 23:21:13 -0400

    more fetch util tests

--- tests/fetch-utils.test.ts
+++ tests/fetch-utils.test.ts
@@ -14,6 +14,18 @@ describe("Fetch Utils", () => {
         expect(responseIsError(mock_response as Response)).toBe(true);
     });
 
+    it("correctly returns false when response is not an error", () => {
+        const mock_response = new Response(
+            JSON.stringify({ "mock": "res" }),
+            {
+                status: 200,
+            }
+        );
+        // cast as node-fetch Response to silence errors requiring additional response fields 
+        expect(responseIsError(mock_response as Response)).toBe(false);
+    });
+
+
     it("correctly returns true when the JSON data is an error object", () => {
         const mock_data = {
             "error": {
@@ -22,5 +34,15 @@ describe("Fetch Utils", () => {
             }
         };
         expect(dataIsError(mock_data)).toBe(true);
+    });
+
+    it("correctly returns false when the JSON data is not an error object", () => {
+        const mock_data = {
+            "data": {
+                "fieldA": "mock",
+                "fieldB": "mock"
+            }
+        };
+        expect(dataIsError(mock_data)).toBe(false);
     });
 })
